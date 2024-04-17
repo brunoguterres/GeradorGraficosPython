1bc7d63f8b375a1b982bd2156b3207aeeac2bdbc
brunoguterres <brunoguterres26@gmail.com>
2024-04-17 14:24:01 -0300

    adição e exclusão de arquivos

--- fluviograma.py
+++ fluviograma.py
@@ -5,18 +5,11 @@ import time
 tempo_inicial = time.time()
 
 path_fluviogramas_obs = 'dados\originais\Fluviogramas_observados.csv'
-path_fluviogramas_sim = 'dados\originais\Fluviogramas_simulados.csv'
+path_fluviogramas_sim = 'dados\originais\Fluviogramas_simulados 1.csv'
 df_fluv_obs = pd.read_csv(path_fluviogramas_obs)
 df_fluv_sim = pd.read_csv(path_fluviogramas_sim)
 
-#print(df_fluv_obs.head())
-#print(df_fluv_sim.head())
-
 datas_x_labels = ['1995-01-01', '2000-01-01', '2005-01-01', '2010-01-01', '2015-01-01']
-
-indice_max_sim = df_fluv_sim['1924'].idxmax()
-linha_max_sim = df_fluv_sim.loc[indice_max_sim]
-print(linha_max_sim)
 
 # ESTAÇÃO: 38830000
 campo_x = 'Data'
@@ -24,11 +17,9 @@ campo_y1 = '38830000'
 campo_y2 = '1924'
 plt.plot(df_fluv_obs[campo_x],
          df_fluv_obs[campo_y1],
-         linestyle=':',
          label='Observada')
 plt.plot(df_fluv_sim[campo_x],
          df_fluv_sim[campo_y2],
-         linestyle='-',
          label='Simulada')
 plt.xlabel('Data')
 plt.ylabel('Vazão')
@@ -40,7 +31,6 @@ tempo_final = time.time()
 print(f'tempo de execução: {tempo_final-tempo_inicial:.1f}s')
 
 plt.xticks(datas_x_labels, rotation=45)
-
 plt.show()
 
 #plt.savefig(f'fluviograma_observado_simulado_{campo_y1}.png')
