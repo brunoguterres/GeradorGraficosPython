49c9b00eaf994312f82b8053f97a1cd597b19205
brunoguterres <brunoguterres26@gmail.com>
2024-04-18 13:21:54 -0300

    Atualização de código

--- fluviogramaEstacao38895000.py
+++ fluviogramaEstacao38895000.py
@@ -8,7 +8,7 @@ plt.rcParams['font.size'] = 10
 plt.rcParams['font.style'] = 'normal'
 plt.rcParams["legend.framealpha"] = 1.0
 plt.rcParams["savefig.dpi"] = 500
-plt.rcParams["lines.linewidth"] = 0.5
+plt.rcParams["lines.linewidth"] = 1
 
 path_fluviogramas_obs = 'dados\originais\Fluviogramas_observados.csv'
 path_fluviogramas_sim = 'dados\originais\Fluviogramas_simulados.csv'
@@ -16,8 +16,8 @@ df_fluv_obs = pd.read_csv(path_fluviogramas_obs)
 df_fluv_sim = pd.read_csv(path_fluviogramas_sim)
 
 # Filtrando os dados para o intervalo desejado
-start_date = '2004-01-01'
-end_date = '2012-12-31'
+start_date = '1994-01-01'
+end_date = '2019-12-31'
 df_fluv_obs = df_fluv_obs[(df_fluv_obs['Data'] >= start_date) & (df_fluv_obs['Data'] <= end_date)]
 df_fluv_sim = df_fluv_sim[(df_fluv_sim['Data'] >= start_date) & (df_fluv_sim['Data'] <= end_date)]
 
@@ -27,7 +27,7 @@ campo_y1 = '38895000'
 campo_y2 = '2039'
 
 # Definindo o tamanho da imagem
-fig, ax = plt.subplots(figsize=(90/25.4, 60/25.4))
+fig, ax = plt.subplots(figsize=(120/25.4, 60/25.4))
 
 # Plotagem dos dados
 ax.plot(df_fluv_obs[campo_x], df_fluv_obs[campo_y1], label='Observada')
@@ -39,17 +39,14 @@ ax.set_title(f'Estação {campo_y1}')
 ax.grid(True)
 
 # Definindo os rótulos desejados (apenas os anos)
-rotulos = ['2004', '2008', '2012']
+rotulos = ['1995', '2000', '2005', '2010', '2015']
 # Obtendo os índices dos pontos correspondentes nos dados
 indices = [df_fluv_obs[df_fluv_obs[campo_x].str.startswith(r)].index[0] for r in rotulos]
 # Ajustando os marcadores no eixo x
 ax.set_xticks(df_fluv_obs[campo_x][indices])
 ax.set_xticklabels(rotulos)
 
-# Limitando a plotagem ao valor de 500 no eixo y
-ax.set_ylim(0, 500)
-
 # Salvando a figura
-plt.savefig(f'graficos/fluviograma_observado_simulado_detalhe.png')
+plt.savefig(f'graficos/fluviograma_observado_simulado_Estacao38895000.png')
 plt.clf()
 print('<<< FINALIZADO >>>')
